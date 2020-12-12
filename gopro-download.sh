081cd6e1d289150642699c25725ac969acf2b871
Edward Moffett <emoffett@users.noreply.github.com>
2020-12-12 15:16:15 +0000

    Add script

---
+++ gopro-download.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+INDEX_URL='http://10.5.5.9:8080/videos/DCIM/100GOPRO/'
+# Try 2 attempts with a 2 second timeout before giving up
+if ! wget -t 2 -T 2 -q $INDEX_URL -O /dev/null; then
+  echo 'Unable to reach the GoPro index page expected at '$INDEX_URL
+  echo 'Ensure that this computer is connected to the GoPros wifi network'
+  exit 1
+fi
+
+DIR="./"$(date -I)
+if [[ ! -d $DIR ]]; then
+  mkdir "$DIR"
+fi
+wget -p "$DIR" -r -A.MP4,.JPG -nc -c $INDEX_URL
