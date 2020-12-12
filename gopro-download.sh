921effcf9897838c24e289a67eeea639b884914d
Edward Moffett <emoffett@users.noreply.github.com>
2020-12-12 16:24:05 +0000

    Start in the correct parent directory

--- gopro-download.sh
+++ gopro-download.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-INDEX_URL='http://10.5.5.9:8080/videos/DCIM/100GOPRO/'
+INDEX_URL='http://10.5.5.9:8080/videos/DCIM/'
 # Try 2 attempts with a 2 second timeout before giving up
 if ! wget -t 2 -T 2 -q $INDEX_URL -O /dev/null; then
   echo 'Unable to reach the GoPro index page expected at '$INDEX_URL
